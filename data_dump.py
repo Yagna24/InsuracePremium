0b03ff7b64e79881185bae1edc52acfa9ee06fef
yagnaa <yagnathakkar1224@gmail.com>
2023-03-13 07:16:26 +0530

    update

--- data_dump.py
+++ data_dump.py
@@ -2,3 +2,8 @@ import pymongo
 import pandas as pd
 import json
 
+
+client = pymongo.MongoClient("mongodb+srv://yagna:wow@cluster0.abo17cz.mongodb.net/?retryWrites=true&w=majority")
+db = client.InsurancePremium
+
+
